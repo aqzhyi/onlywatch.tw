96f57b065b1c37f988b63b08f45462dd5f93091a
ねむけ <pleasurazy@gmail.com>
2025-08-15 20:22:47 +0800

    feat!: redesign "/calendar/page" for better phone experience
    - group the date cards by country to improve the browsing experience
    - all events for that date will be expanded in the drawer
    
    ---
    
    WHY
    
    i want to always show events from the past N days so that users can review what has happened, but i also want to quickly find the group of events for today
    
    the original layout is not very suitable for mobile browsing, as long events rows that do not effectively guide users to the date they want to see
    
    ---
    
    BREAKING CHANGE: the UI of app/calendar/page has been redesigned now

---
+++ @apps/onlywatch.tw/src/app/(main)/calendar/page.tsx
@@ -0,0 +1,44 @@
+import { DayCard } from '~/features/jin10/components/DayCard'
+import { findManyEvents } from '~/features/jin10/db/findManyEvents'
+import { days } from '~/utils/days'
+
+export const revalidate = 180
+
+export default async function Page() {
+  const FROM_DAYS = -1
+  const END_DAYS = 15
+
+  const events = await findManyEvents({
+    startOf: days().add(FROM_DAYS, 'days').startOf('days').toISOString(),
+    endOf: days()
+      .add(END_DAYS + 1, 'days')
+      .endOf('days')
+      .toISOString(),
+  })
+
+  return (
+    <div className='grid grid-cols-1 gap-2'>
+      {Object.entries(events.dataGroupedByDate || {}).map(
+        ([dayAt, dayEvents]) => {
+          const today = days().startOf('day')
+
+          return (
+            <DayCard
+              key={dayAt}
+              dayAt={dayAt}
+              value={dayEvents}
+              variant={
+                new Map([
+                  [days(dayAt).isBefore(today), 'past' as const],
+                  [days(dayAt).isAfter(today), 'upcoming' as const],
+                  [[0, 6].includes(days(dayAt).weekday()), 'holiday' as const],
+                  [today.format('YYYY-MM-DD') === dayAt, 'today' as const],
+                ]).get(true) || 'default'
+              }
+            />
+          )
+        },
+      )}
+    </div>
+  )
+}
