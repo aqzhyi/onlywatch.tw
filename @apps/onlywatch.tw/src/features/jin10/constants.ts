8495ebdea89fc3fcac5cf2588491f1b1c163b77d
ねむけ <pleasurazy@gmail.com>
2025-09-11 02:48:03 +0800

    feat: add main refinancing rate term mapping for rate decisions
    - added mapping from "主要再融資利率" to "利率決議" in financialTermDict

--- @apps/onlywatch.tw/src/features/jin10/constants.ts
+++ @apps/onlywatch.tw/src/features/jin10/constants.ts
@@ -5,6 +5,7 @@ export const constants = {
    */
   timezoneOffset: -6,
   financialTermDict: [
+    { from: /主要再融資利率/gi, to: '利率決議' },
     { from: /利率決定/gi, to: '利率決議' },
     { from: /目標利率/gi, to: '利率決議' },
     { from: /(普京|普亭)/gi, to: '普丁' },
