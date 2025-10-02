75061a230927e429a6407087d7d834f47b78e24c
ねむけ <pleasurazy@gmail.com>
2025-10-03 00:26:17 +0800

    refactor: remove unused exports from `use-catch-all-next-params` package
    💬 multi-entry point / no large barrel files
    💬 improve tree shaking
    
    - remove `buildUrlFromTemplate` export
    - remove `parseUrlByTemplate` export

--- @packages/use-catch-all-next-params/src/index.ts
+++ @packages/use-catch-all-next-params/src/index.ts
@@ -1,4 +1,2 @@
 export { useCatchAllNextParams } from './hooks/useCatchAllNextParams'
 export type { UseCatchAllNextParamsReturn } from './hooks/useCatchAllNextParams'
-export { buildUrlFromTemplate } from './utils/buildUrlFromTemplate'
-export { parseUrlByTemplate } from './utils/parseUrlByTemplate'
