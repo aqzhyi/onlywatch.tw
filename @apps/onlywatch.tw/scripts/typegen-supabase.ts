6b6ff9c445a3ef9d250e06847f5e7f8265fde228
ねむけ <pleasurazy@gmail.com>
2025-12-02 23:35:28 +0800

    fix(onlywatch): correct output path in `typegen-supabase.ts`
    - change output path from `./src/db/database.types.ts` to `./app/db/database.types.ts`

--- @apps/onlywatch.tw/scripts/typegen-supabase.ts
+++ @apps/onlywatch.tw/scripts/typegen-supabase.ts
@@ -9,7 +9,7 @@ try {
   const dbUrl = `postgresql://postgres.${process.env.SUPABASE_PROJECT_ID}:${process.env.SUPABASE_DB_PASSWORD}@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres`
 
   const { stdout: genSupabaseTypesResult } =
-    await $`pnpm exec supabase gen types typescript --db-url ${dbUrl} > ./src/db/database.types.ts`
+    await $`pnpm exec supabase gen types typescript --db-url ${dbUrl} > ./app/db/database.types.ts`
 
   console.info(genSupabaseTypesResult || 'supabase gen OK')
 } catch (error) {
